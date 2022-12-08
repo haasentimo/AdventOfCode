a65bec453d2833bc2eb89026ab046e6529b1a135
haasentimo <haasentimo@gmail.com>
2022-12-08 21:20:08 +0100

    Day 2 challenges

---
+++ Day2/Task2.py
@@ -0,0 +1,34 @@
+lost = 0
+draw = 3
+win = 6
+
+rock = 1
+paper = 2
+scissor = 3
+
+with open("input.txt") as file:
+    total_score = 0
+    for line in file.readlines():
+        opponent, you = line.replace("\n", "").split(" ")
+        if opponent == "A" and you == "X":
+            total_score += lost + scissor
+        elif opponent == "A" and you == "Y":
+            total_score += draw + rock
+        elif opponent == "A" and you == "Z":
+            total_score += win + paper
+        elif opponent == "B" and you == "X":
+            total_score += lost + rock
+        elif opponent == "B" and you == "Y":
+            total_score += draw + paper
+        elif opponent == "B" and you == "Z":
+            total_score += win + scissor
+        elif opponent == "C" and you == "X":
+            total_score += lost + paper
+        elif opponent == "C" and you == "Y":
+            total_score += draw + scissor
+        elif opponent == "C" and you == "Z":
+            total_score += win + rock
+        else:
+            print("this is bad")
+
+print(total_score)
